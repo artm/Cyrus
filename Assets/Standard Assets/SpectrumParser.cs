aa7f05117819f03271bcbab422f0e1df4a053916
Artem Baguinski <artm@v2.nl>
2011-11-19 15:11:53 +0100

    "amplifier"

--- Assets/Standard Assets/SpectrumParser.cs
+++ Assets/Standard Assets/SpectrumParser.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class SpectrumParser : MonoBehaviour {
+	public float mul = 1;
+
 	void OnOscMessage(object[] msg)
 	{
 		string addr = msg[0] as string;
@@ -11,7 +13,7 @@ public class SpectrumParser : MonoBehaviour {
 
 		float[] spectrum = new float[(msg.Length-3)/2];
 		for(int i = 0; i<spectrum.Length; i++)
-			spectrum[i] = (float)msg[3+2*i];
+			spectrum[i] = mul * (float)msg[3+2*i];
 		BroadcastMessage("OnAudioSpectrum", spectrum, SendMessageOptions.DontRequireReceiver);
 	}
 }
