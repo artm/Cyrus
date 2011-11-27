d1ce614afaf9f119ca71339ba6a09cf3af458176
Artem Baguinski <artm@v2.nl>
2011-11-27 09:44:37 +0100

    cleanup vehicle

--- Assets/Standard Assets/Mirk/Vehicle.cs
+++ Assets/Standard Assets/Mirk/Vehicle.cs
@@ -6,12 +6,6 @@ public class Vehicle : MonoBehaviour {
 	public float speed = 1;
 	public float rotSpeed = 1;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
 		transform.Translate( 0, 0, speed * Time.deltaTime );
 		transform.Rotate(- rotSpeed * Input.GetAxis("Vertical") * Time.deltaTime,
